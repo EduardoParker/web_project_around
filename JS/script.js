342f94e3d0f2fb0c2a08a7a8c1571a2a90a72d11
EduardoParker <ingeparker555@gmail.com>
2024-06-25 18:51:01 -0600

    se agrega popup form image

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,38 @@
-let popup = document.querySelector(".popup");
-let profileButton = document.querySelector(".profile__edit-button");
-let profileCloseButton = document.querySelector(".form__close-icon");
-let profileSaveButton = document.querySelector(".form__button_submit");
+const popup = document.querySelector(".popup");
+const profileButton = document.querySelector(".profile__edit-button");
+const profileCloseButton = document.querySelector(".form__close-icon");
+const profileSaveButton = document.querySelector(".form__button_submit");
+const imageAddButton = document.querySelector(".profile__add-button");
+const formImage = document.querySelector("#form-image");
 
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+  },
+];
+
+// apartado para el popup profile
 profileButton.addEventListener("click", openProfile);
 
 function openProfile() {
@@ -36,3 +66,16 @@ function handleProfileFormSubmit(evt) {
   profileJob.textContent = `${jobInput}`;
 }
 formElement.addEventListener("submit", handleProfileFormSubmit, SaveAndClose);
+
+// apartado para el popup imagenes
+imageAddButton.addEventListener("click", openImageForm);
+
+function openImageForm() {
+  formImage.classList.add("popup_opened-form");
+}
+
+profileCloseButton.addEventListener("click", closeImageForm);
+
+function closeImageForm() {
+  formImage.classList.remove("popup_opened-form");
+}
