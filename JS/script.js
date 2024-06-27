d4b149a6485347c709505c33533b820c144716cd
EduardoParker <ingeparker555@gmail.com>
2024-06-26 18:48:14 -0600

    se termina de desarrollar apartado de popup tarjetas

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,15 @@
-const popup = document.querySelector(".popup");
+const popup = document.querySelector(".popup__profile");
 const profileButton = document.querySelector(".profile__edit-button");
-const profileCloseButton = document.querySelector(".form__close-icon");
+const profileCloseButton = document.querySelector(".form__close-icon_profile");
 const profileSaveButton = document.querySelector(".form__button_submit");
 const imageAddButton = document.querySelector(".profile__add-button");
-const formImage = document.querySelector("#form-image");
+const formImage = document.querySelector(".popup__form-image");
+const formImageCloseButton = document.querySelector(
+  ".form__close-icon_form-image"
+);
+const templateNode = document.querySelector(".template");
+const elementArea = document.querySelector(".elements");
+const imageSaveButton = document.querySelector(".form__button_submit_image");
 
 const initialCards = [
   {
@@ -32,6 +38,16 @@ const initialCards = [
   },
 ];
 
+// para agregar dinamicamente las imagenes
+initialCards.forEach(function (item) {
+  const newNode = templateNode.content
+    .querySelector(".element")
+    .cloneNode(true);
+  newNode.querySelector(".element__image").src = item.link;
+  newNode.querySelector(".element__description").textContent = item.name;
+  elementArea.append(newNode);
+});
+
 // apartado para el popup profile
 profileButton.addEventListener("click", openProfile);
 
@@ -51,19 +67,21 @@ function SaveAndClose() {
   popup.classList.remove("popup_opened");
 }
 
-let formElement = document.querySelector(".popup__form");
+// seccion para la modificacion del perfil
+const formElement = document.querySelector(".popup__form");
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
 
-  let nameInput = document.querySelector("#name").value;
-  let jobInput = document.querySelector("#about-me").value;
+  const nameInput = document.querySelector("#name").value;
+  const jobInput = document.querySelector("#about-me").value;
 
-  let profileName = document.querySelector(".profile__name");
-  let profileJob = document.querySelector(".profile__info-aboutme");
+  const profileName = document.querySelector(".profile__name");
+  const profileJob = document.querySelector(".profile__info-aboutme");
 
   profileName.textContent = `${nameInput}`;
   profileJob.textContent = `${jobInput}`;
+  formElement.reset();
 }
 formElement.addEventListener("submit", handleProfileFormSubmit, SaveAndClose);
 
@@ -71,11 +89,40 @@ formElement.addEventListener("submit", handleProfileFormSubmit, SaveAndClose);
 imageAddButton.addEventListener("click", openImageForm);
 
 function openImageForm() {
-  formImage.classList.add("popup_opened-form");
+  formImage.classList.add("popup_opened");
 }
 
-profileCloseButton.addEventListener("click", closeImageForm);
+formImageCloseButton.addEventListener("click", closeImageForm);
 
 function closeImageForm() {
-  formImage.classList.remove("popup_opened-form");
+  formImage.classList.remove("popup_opened");
 }
+// seccion para la modificacion de las imagenes
+imageSaveButton.addEventListener("click", imageSaveAndClose);
+
+function imageSaveAndClose() {
+  formImage.classList.remove("popup_opened");
+}
+
+const formImageElement = document.querySelector(".form__image");
+
+function handleImageFormSubmit(evt) {
+  evt.preventDefault();
+
+  const titleInput = document.querySelector("#title").value;
+  const linkInput = document.querySelector("#url").value;
+  const newNode = templateNode.content
+    .querySelector(".element")
+    .cloneNode(true);
+
+  newNode.querySelector(".element__description").textContent = titleInput;
+  newNode.querySelector(".element__image").src = linkInput;
+  elementArea.prepend(newNode);
+  formImageElement.reset();
+}
+
+formImageElement.addEventListener(
+  "submit",
+  handleImageFormSubmit,
+  imageSaveAndClose
+);
